b5fe84047ac2827845cb0ff98e2ffc707b7b1e75
Tony Azersky <anthony.azersky@gmail.com>
2016-05-15 20:50:55 -0400

    Refactor point store into its own class

--- app/components/Dartboard.jsx
+++ app/components/Dartboard.jsx
@@ -1,39 +1,11 @@
 import React from 'react';
 import Scoreboard from './Scoreboard.jsx';
 import ScoreRow from './ScoreRow.jsx';
+import { getPointStore } from './PointStore.jsx';
 import { createStore } from 'redux';
 
 var points = ['20', '19', '18', '17', '16', '15', 'B'];
-
-const pointIncrementer = (state = {left: 0, right:0}, action) => {
-  var newState;
-  if (action.type === 'ADD_POINTS') {
-    if (action.side === 'left') {
-      newState = {
-        left: state['left'] + action.num_points,
-        right: state['right']
-      };
-    } else if (action.side === 'right') {
-      newState = {
-        left: state['left'],
-        right: state['right'] + action.num_points
-      };
-    } else {
-      console.log('Invalid side');
-    }
-  } else {
-    console.log('Couldn\'t find a match!');
-    newState = state;
-  }
-  console.log('*****************************');
-  console.log('Old state: ', state);
-  console.log('Got an action: ', action);
-  console.log('New state: ', newState);
-  return newState;
-}
-
-var pointStore = createStore(pointIncrementer);
-
+var pointStore = getPointStore();
 class Dartboard extends React.Component {
 
   constructor(props) {
