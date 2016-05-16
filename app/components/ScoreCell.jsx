28b863e97f36ad7db7434c3805d78d699bd29abc
Tony Azersky <anthony.azersky@gmail.com>
2016-05-15 20:13:54 -0400

    Factored out ScoreCell, but broke checking for closing out

---
+++ app/components/ScoreCell.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+
+class ScoreCell extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      points: 0,
+      symbols: ["-", "/", "X", "O"]
+    };
+  }
+
+  registerHit() {
+    var side=this.props.side;
+    if (this.state.points < 3) {
+      this.setState({points: this.state.points + 1});
+    } else {
+      if (!this.closedOut())
+      this.props.pointStore.dispatch({
+        type: 'ADD_POINTS',
+        side: side,
+        num_points: this.props.point==='B' ? 25 : Number(this.props.point)
+      });
+    }
+
+    return null;
+  }
+
+  closedOut() {
+    if (this.state.points['left'] >= 3 && this.state.points['right'] >= 3) {
+      return true;
+    }
+    return false;
+  }
+
+  render() {
+    return (
+        <div className="rowCell"><button onClick={this.registerHit.bind(this)} className="left">
+          {this.state.symbols[this.state.points]}
+        </button></div>
+    );
+  }
+}
+
+export default ScoreCell;
--- app/components/ScoreRow.jsx
+++ app/components/ScoreRow.jsx
@@ -1,63 +1,18 @@
 import React from 'react';
+import ScoreCell from './ScoreCell.jsx';
 
 class ScoreRow extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = {
-      points: {
-        left: 0,
-        right: 0
-      },
-      symbols: ["-", "/", "X", "O"]
-    };
-    this.hitLeft = this.hitLeft.bind(this);
-    this.hitRight = this.hitRight.bind(this);
-  }
-
-  registerHit(side) {
-    var side=arguments[0];
-    if (this.state.points[side] < 3) {
-      var newPoints = this.state.points;
-      newPoints[side] = newPoints[side] + 1;
-      this.setState({points: newPoints});
-    } else {
-      if (!this.closedOut())
-      this.props.pointStore.dispatch({
-        type: 'ADD_POINTS',
-        side: side,
-        num_points: this.props.point==='B' ? 25 : Number(this.props.point)
-      });
-    }
-
-    return null;
-  }
-
-  closedOut() {
-    if (this.state.points['left'] >= 3 && this.state.points['right'] >= 3) {
-      return true;
-    }
-    return false;
-  }
-
-  hitLeft() {
-    this.registerHit("left");
-  }
-
-  hitRight() {
-    this.registerHit("right");
   }
 
   render() {
     return (
       <div className="row">
-        <div className="rowCell"><button onClick={this.hitLeft} className="left">
-          {this.state.symbols[this.state.points['left']]}
-        </button></div>
+        <ScoreCell side='left' point={this.props.point} pointStore={this.props.pointStore} />
         <div className="mid rowCell">{this.props.point}</div>
-        <div className="rowCell"><button onClick={this.hitRight} className="right">
-          {this.state.symbols[this.state.points['right']]}
-        </button></div>
+        <ScoreCell side='right' point={this.props.point} pointStore={this.props.pointStore} />
       </div>
     );
   }
