19761a7523f3be0dbdfe2cfdb92f27fae6cea742
Tony Azersky <anthony.azersky@gmail.com>
2016-05-15 21:00:56 -0400

    Refactor the store a little bit

--- app/components/PointStore.jsx
+++ app/components/PointStore.jsx
@@ -5,26 +5,27 @@ import { createStore } from 'redux';
 
 var points = ['20', '19', '18', '17', '16', '15', 'B'];
 
+const addPoint = (state, action) => {
+  var newState = JSON.parse(JSON.stringify(state));
+  if (action.side === 'left') {
+    newState['left'] = state['left'] + action.num_points;
+  } else if (action.side === 'right') {
+    newState['right'] = state['right'] + action.num_points;
+  } else {
+    console.log('Invalid side');
+  }
+  return newState;
+}
+
 const pointIncrementer = (state = {left: 0, right:0}, action) => {
   var newState;
   if (action.type === 'ADD_POINTS') {
-    if (action.side === 'left') {
-      newState = {
-        left: state['left'] + action.num_points,
-        right: state['right']
-      };
-    } else if (action.side === 'right') {
-      newState = {
-        left: state['left'],
-        right: state['right'] + action.num_points
-      };
-    } else {
-      console.log('Invalid side');
-    }
+    newState = addPoint(state, action);
   } else {
     console.log('Couldn\'t find a match!');
     newState = state;
   }
+
   console.log('*****************************');
   console.log('Old state: ', state);
   console.log('Got an action: ', action);
